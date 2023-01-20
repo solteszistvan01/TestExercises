3a80a328de985271355815da244e03510019c6dd
solteszistvan <solteszistvan@outlook.com>
2023-01-20 17:49:13 +0100

    Initial commit
    Testing has not been done.
    Waiting for branches to become apparent.

---
+++ test1.cpp
@@ -0,0 +1,9 @@
+//
+// Created by solteszistvan on 2023.01.20..
+//
+#include <gtest/gtest.h>
+//#include "/home/solteszistvan/CLionProjects/GoogleTest/lib/googletest-main/googletest/include/gtest/gtest.h"
+
+TEST(TestSuite, Iterators_Functions){
+    EXPECT_EQ(0,0);
+}
---
+++ tst/test1.cpp
@@ -0,0 +1,5 @@
+//
+// Created by solteszistvan on 2023.01.20..
+//
+
+#include "test1.h"
