bd7c1a5f784d53937a92aef1b3410cffcf12ac26
solteszistvan <solteszistvan@outlook.com>
2023-01-30 20:58:52 +0100

    First commit

--- matrix_Test.cpp
+++ matrix_Test.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "matrix.h"
 
-//TODO: Find a directory structure for the project
+//TODO: Find a directory structure for the project.
 //TODO: Setup a directory for the project for test cases using Gtest
 //TODO: Make it sure that the project is contained in a git repo containing all the projects that are practices for the testing environment!
 float init1(int i, int j){
